b712e64c5b32db3719a2e60444e7848cc00d0e98
satinder <satindersingh472@gmail.com>
2022-08-26 14:52:58 -0600

    restaurant registration successful

--- src/views/restaurantSignUp.vue
+++ src/views/restaurantSignUp.vue
@@ -4,49 +4,90 @@
     <div class="form">
       <div class="content_item">
         <p>Name</p>
-        <input type="text" placeholder="Restaurant's name" />
+        <input type="text" placeholder="Restaurant's name" ref="name" />
       </div>
       <div class="content_item">
         <p>Addess</p>
-        <input type="url" placeholder="Restaurant's address" />
+        <input type="url" placeholder="Restaurant's address" ref="address" />
       </div>
       <div class="content_item">
         <p>City</p>
-        <input type="text" placeholder="city" />
+        <input type="text" placeholder="city" ref="city" />
       </div>
       <div class="content_item">
         <p>Phone Number</p>
-        <input type="tel" placeholder="phone" />
+        <input type="tel" placeholder="phone" ref="phone_number" />
       </div>
     </div>
     <div class="content_item">
       <p>Email</p>
-      <input type="text" placeholder="email" />
+      <input type="text" placeholder="email" ref="email" />
     </div>
     <div class="content_item">
       <p>Password</p>
-      <input type="password" placeholder="password" />
+      <input type="password" placeholder="password" ref="password" />
     </div>
     <div class="content_item">
       <p>Bio</p>
-      <textarea name="textarea"></textarea>
+      <textarea ref="bio" name="textarea"></textarea>
     </div>
     <div class="content_item">
       <p>Profile URL</p>
-      <input type="url" placeholder="profile URL" />
+      <input type="url" placeholder="profile URL" ref="profile_url" />
     </div>
     <div class="content_item">
       <p>Banner URL</p>
-      <input type="url" placeholder="banner URL" />
+      <input type="url" placeholder="banner URL" ref="banner_url" />
     </div>
-    <input class="submit" type="submit" value="Register" />
+   <button class="submit" @click="send_request">Register</button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import cookies from "vue-cookies";
 import foodieHeader from "@/components/foodieHeader.vue";
 export default {
-  components: { foodieHeader },
+  components: {
+    foodieHeader,
+  },
+  methods: {
+    send_request() {
+      axios
+        .request({
+          url: `https://innotechfoodie.ml/api/restaurant`,
+          method: `POST`,
+          headers: {
+            "x-api-key": "TVTZDiQZDzjkWqVkNCxr",
+          },
+          data: {
+            name: this.$refs[`name`][`value`],
+            address: this.$refs[`address`][`value`],
+            city: this.$refs[`city`][`value`],
+            phone_number: this.$refs[`phone_number`][`value`],
+            email: this.$refs[`email`][`value`],
+            password: this.$refs[`password`][`value`],
+            bio: this.$refs[`bio`][`value`],
+            profile_url: this.$refs[`profile_url`][`value`],
+            banner_url: this.$refs[`banner_url`][`value`]
+          },
+        })
+        .then((response) => {
+          if(response){
+            cookies.set(`restaurant_id` , response[`data`][`restaurantId`]);
+            cookies.set(`token`, response[`data`][`token`]);
+          }
+        })
+        .catch((error) => {
+          error;
+        });
+    },
+  },
+  data() {
+    return {
+      details: undefined,
+    };
+  },
 };
 </script>
 
@@ -82,7 +123,8 @@ export default {
     justify-self: start;
     padding: 5px;
   }
-  input,textarea {
+  input,
+  textarea {
     font-size: 0.8rem;
     width: 100%;
     border-radius: 10px;
