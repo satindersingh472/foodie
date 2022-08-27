ef13b887caec4f0adc20d9b76426f22807d06aa3
satinder <satindersingh472@gmail.com>
2022-08-27 15:38:43 -0600

    done with displaying menu of each restaurant on client side

--- src/views/addItems.vue
+++ src/views/addItems.vue
@@ -1,20 +1,82 @@
 <template>
-    <div>
-this is add items
+  <div class="main_form" >
+    <h2>Add Items to the Menu <br> for your Restaurant</h2>
+    <div class="form">
+      <div class="content_item">
+        <h4>Product Name:</h4>
+        <input type="text" ref="name" />
+      </div>
+      <div class="content_item">
+        <h4>Product Description:</h4>
+        <input type="text" ref="description" />
+      </div>
+      <div class="content_item">
+        <h4>Product Image URL: </h4>
+        <input type="url" ref="image_url">
+      </div>
+      <div class="content_item">
+        <h4>Product Price:</h4>
+        <input type="text" ref="price">
+      </div>
+      <button @click="send_request">Add Items</button>
     </div>
+    
+  </div>
 </template>
 
 <script>
-    export default {
-        methods: {
-            send_request() {
-                
-            }
-        },
-        
-    }
+import axios from "axios";
+import cookies from "vue-cookies";
+export default {
+  methods: {
+    send_request() {
+      axios
+        .request({
+          url: ` https://innotechfoodie.ml/api/menu`,
+          method: `POST`,
+          headers: {
+            "x-api-key": "TVTZDiQZDzjkWqVkNCxr",
+            token: cookies.get(`token`)
+          },
+          data: {
+            description: this.$refs[`description`][`value`],
+            image_url: this.$refs[`image_url`][`value`],
+            name: this.$refs[`name`][`value`],
+            price: this.$refs[`price`][`value`],
+          },
+        })
+        .then((response) => {
+          response;
+        })
+        .catch((error) => {
+          error;
+        });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
+*{
+    padding: 0px;
+    margin: 0px;
+    text-align: center;
+}
+.main_form{
+    display: grid;
+    gap: 20px;
+}
+.form{
+    display: grid;
+    gap: 10px;
+    .content_item{
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        place-items: center;
+        h4{
+            display: grid;
+            text-align: start;
+        }
+    }
+}
 </style>
--- src/views/viewMenu.vue
+++ src/views/viewMenu.vue
@@ -1,37 +1,66 @@
 <template>
-    <div>
-        <h1>this is menu profile page</h1>
-       <div v-if="info !== undefined">  
-        <h1>{{info}}</h1>
-       </div>
+  <div>
+    <h1>this is menu profile page</h1>
+    <div v-if="info !== undefined">
+      <div class="content_item" v-for="detail in details" :key="detail[`id`]">
+        <img
+          class="content_item_image"
+          :src="detail[`image_url`]"
+          :alt="`image of ${detail[`name`]}`"
+        />
+        <p>{{ detail[`description`] }}</p>
+        <div class="content_item_details">
+          <p>{{ detail[`name`]}}</p>
+          <p>{{detail[`price`]}}</p>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 import cookies from "vue-cookies";
-    export default {
-
-        mounted () {
- this.info = cookies.get(`restaurant_id`);
-            axios.request({
-                url: `https://innotechfoodie.ml/api/menu`,
-                headers:{
-                    'x-api-key': 'TVTZDiQZDzjkWqVkNCxr'
-                },
-                params:{
-                    restaurant_id: this.info
-                }
-            }).then((response)=>{response}).catch((error)=>{error});
+export default {
+  mounted() {
+    this.info = cookies.get(`restaurant_id`);
+    axios
+      .request({
+        url: `https://innotechfoodie.ml/api/menu`,
+        headers: {
+          "x-api-key": "TVTZDiQZDzjkWqVkNCxr",
         },
-        data() {
-            return {
-                info: undefined
-            }
+        params: {
+          restaurant_id: this.info,
         },
-    }
+      })
+      .then((response) => {
+        this.details = response[`data`];
+        for(let i =0; i<response[`data`].length;i++){
+            this.details[i][`price`] = response[`data`][i][`price`].toFixed(2);
+        }
+      })
+      .catch((error) => {
+        error;
+      });
+  },
+  data() {
+    return {
+      info: undefined,
+      details: undefined,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+*{
+    padding: 0px;
+    margin: 0px;
+}
+img{
+    width: 100%;
+    height: 250px;
+}
 
 </style>
