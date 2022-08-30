62048d9511e1bec9072878043b299dc8646ccda4
satinder <satindersingh472@gmail.com>
2022-08-30 14:49:36 -0600

    working on client cart

--- src/views/clientCart.vue
+++ src/views/clientCart.vue
@@ -9,15 +9,12 @@
         <img :src="order_detail[`image_url`]" />
         <h2>{{ order_detail[`name`] }}</h2>
         <p>{{ order_detail[`price`] }}</p>
-        <p>ID: {{order_detail[`id`]}}</p>
+        <p>ID:{{order_detail[`id`]}}</p>
         <button @click="delete_item(index, $event)">Remove</button>
       </div>
     </div>
     <div class="place_order">
         <button @click="add_items" >Place Order</button>
-        <div v-for="(selected,index) in selected_items" :key="index" >
-            <h2>{{selected[`name`]}}</h2>
-        </div>
     </div>
   </div>
 </template>
@@ -33,16 +30,10 @@ export default {
       this.order_details.splice(index, 1);
       cookies.set(`orders`, JSON.stringify(this.order_details));
     },
-    add_items(){
-     for(let i=0;i<this.order_details.length;i++){
-      this.selected_items.push(this.order_details[i]);
-     }
-    }
   },
   data() {
     return {
-      order_details: undefined,
-      selected_items: undefined
+      order_details: undefined
     };
   },
 };
