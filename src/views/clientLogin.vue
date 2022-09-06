5a0b5a812b9c03b591ce1df8073f506b6ff42ec3
satinder <satindersingh472@gmail.com>
2022-09-06 10:38:00 -0600

    working on styliing

--- src/views/clientLogin.vue
+++ src/views/clientLogin.vue
@@ -137,4 +137,10 @@ export default {
     }
   }
 }
+@media only screen and (min-width:500px){
+  .main_login{
+    width: 500px;
+    text-align: center;
+  }
+}
 </style>
--- src/views/clientSignUp.vue
+++ src/views/clientSignUp.vue
@@ -1,51 +1,56 @@
 <template>
-<!-- client signup component is for registering the client -->
-  <div class="main_sign_up_form">
-    <foodie-header></foodie-header>
-    <div class="registration">
-      <h2>Register as a Client</h2>
-      <!-- if already a user there is a login link  -->
-      <p>Already a User?<router-link to="client_login">LogIn</router-link></p>
-      <div class="form">
-        <div class="information">
-          <!-- this form will have all the input fields required to signup  -->
-          <div>
-            <p>User Name</p>
-            <input type="text" ref="username" placeholder="username" />
+  <div class="main_page">
+    <!-- client signup component is for registering the client -->
+    <div class="main_sign_up_form">
+      <foodie-header></foodie-header>
+      <div class="registration">
+        <h2>Register as a Client</h2>
+        <!-- if already a user there is a login link  -->
+        <p>
+          Already a User?
+          <router-link to="client_login">LogIn</router-link>
+        </p>
+        <div class="form">
+          <div class="information">
+            <!-- this form will have all the input fields required to signup  -->
+            <div>
+              <p>User Name</p>
+              <input type="text" ref="username" placeholder="username" />
+            </div>
+            <div>
+              <p>Password</p>
+              <input type="text" ref="password" placeholder="password" />
+            </div>
+            <div>
+              <p>First Name</p>
+              <input type="text" ref="first_name" placeholder="first name" />
+            </div>
+            <div>
+              <p>Last Name</p>
+              <input type="text" ref="last_name" placeholder="last_name" />
+            </div>
           </div>
-          <div>
-            <p>Password</p>
-            <input type="text" ref="password" placeholder="password" />
+          <div class="email">
+            <p>Email</p>
+            <input type="text" ref="email" placeholder="Email" />
           </div>
-          <div>
-            <p>First Name</p>
-            <input type="text" ref="first_name" placeholder="first name" />
+          <div class="image">
+            <p>Profile Image</p>
+            <input type="url" ref="file" placeholder="url for the profile image" />
           </div>
-          <div>
-            <p>Last Name</p>
-            <input type="text" ref="last_name" placeholder="last_name" />
-          </div>
+          <!-- register button will call the api-->
+          <button @click="signup">REGISTER</button>
+          <p v-if="message !== undefined">{{ message }}</p>
         </div>
-        <div class="email">
-          <p>Email</p>
-          <input type="text" ref="email" placeholder="Email" />
-        </div>
-        <div class="image">
-          <p>Profile Image</p>
-          <input type="url" ref="file">
-        </div>
-        <!-- register button will call the api-->
-        <button @click="signup">REGISTER</button>
-        <p v-if="message !== undefined">{{message}}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from "axios";
-import cookies from "vue-cookies";
-import FoodieHeader from '@/components/foodieHeader.vue';
+import axios from 'axios'
+import cookies from 'vue-cookies'
+import FoodieHeader from '@/components/foodieHeader.vue'
 export default {
   components: {
     FoodieHeader,
@@ -57,11 +62,11 @@ export default {
         .request({
           // endpoint used for signup
           url: `https://innotechfoodie.ml/api/client`,
-          // post method is used 
+          // post method is used
           method: `POST`,
           // api key is used as headers
           headers: {
-            "x-api-key": "TVTZDiQZDzjkWqVkNCxr",
+            'x-api-key': 'TVTZDiQZDzjkWqVkNCxr',
           },
           // the following data is used from the form to send as a data
           data: {
@@ -70,33 +75,33 @@ export default {
             username: this.$refs[`username`][`value`],
             image_url: this.$refs[`image_url`][`value`],
             first_name: this.$refs[`first_name`][`value`],
-            last_name: this.$refs[`last_name`][`value`]
+            last_name: this.$refs[`last_name`][`value`],
           },
         })
         .then((response) => {
           // if response is successfull then token and client cookies are set
-          cookies.set(`token`, response[`data`][`token`]);
-          cookies.set(`client_id`, response[`data`][`client_id`]);
+          cookies.set(`token`, response[`data`][`token`])
+          cookies.set(`client_id`, response[`data`][`client_id`])
           // the user will be routed to discover restaurants
-          this.$router.push(`/discover_restaurants`);
+          this.$router.push(`/discover_restaurants`)
         })
         .catch((error) => {
           // if user exists then message will be displayed
-          if(error[`response`][`status`] === 409 ){
-            this.message = `User Already Exists`;
-          } else if(error) {
+          if (error[`response`][`status`] === 409) {
+            this.message = `User Already Exists`
+          } else if (error) {
             // if there is any other error than the following message will get displayed
-            this.message = `Please fill out all the Required fields`;
+            this.message = `Please fill out all the Required fields`
           }
-        });
+        })
     },
   },
   data() {
     return {
-      message: undefined
-    };
+      message: undefined,
+    }
   },
-};
+}
 </script>
 
 <style lang="scss" scoped>
@@ -112,11 +117,9 @@ export default {
     width: 100%;
     margin: 10px;
     border-radius: 5px;
-    background-color: red;
-    color: white;
-    font: 1.2rem white;
-    font-family: "Courier New", Courier, monospace;
-  }
+    background-color: #f79797;
+    font: 1.2rem;
+    padding: 5px;  }
   .information {
     width: 100%;
     display: grid;
@@ -129,7 +132,6 @@ export default {
         align-self: end;
         justify-self: start;
         margin: 5px 0px 5px 0px;
-        font-size: 0.8rem;
       }
       input {
         width: 100%;
@@ -137,23 +139,36 @@ export default {
         justify-self: center;
         border: 2px solid black;
         border-radius: 5px;
+        font-size: 1.5rem;
+        background-color: #b0cad8;
       }
     }
   }
-  .email,.image {
+  .email,
+  .image {
     display: grid;
     place-items: start;
     width: 100%;
     p {
       margin: 5px 0px 5px 0px;
-      font-size: 0.8rem;
     }
     input {
       width: 100%;
+      font-size: 1.5rem;
       justify-self: center;
       border: 2px solid black;
       border-radius: 5px;
+      background-color: #b0cad8;
     }
+  }
+}
+@media only screen and (min-width: 500px) {
+  .main_page {
+    display: grid;
+  }
+  .main_sign_up_form {
+    justify-self: center;
+    width: 500px;
   }
 }
 </style>
