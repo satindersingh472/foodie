a8f93afd3ce265c3fc6c616f4dadf16c7d7d56bc
satindersingh772 <92008199+satindersingh772@users.noreply.github.com>
2022-10-23 23:22:02 -0600

    worked_on_logout_component

--- src/components/allLogout.vue
+++ src/components/allLogout.vue
@@ -1,31 +1,54 @@
 <template>
-<!-- if show component is not false then the following div will show up -->
+  <!-- if show component is not false then the following div will show up -->
   <div v-if="show_component !== false">
     <!-- at click will trigger delete token function -->
     <button @click="delete_token">LogOut</button>
+    <div  v-if="message !== undefined" >{{message}}</div>
   </div>
 </template>
 
 <script>
 // cookies are imported
-import cookies from "vue-cookies";
+import cookies from 'vue-cookies'
 export default {
   methods: {
     // delete token method will check for client and restaurant cookies and delete whichever is present
     delete_token() {
       if (cookies.get(`client_id`)) {
-        cookies.remove(`client_id`);
-        cookies.remove(`token`);
-        cookies.remove(`restaurant_selected`);
-        this.$router.push(`/`);
+        cookies.remove(`client_id`)
+        cookies.remove(`token`)
+        cookies.remove(`restaurant_selected`)
+        this.$router.push(`/`)
       } else if (cookies.get(`restaurant_id`)) {
-        cookies.remove(`restaurant_id`);
-        cookies.remove(`token`);
-        this.$router.push(`/`);
+        cookies.remove(`restaurant_id`)
+        cookies.remove(`token`)
+        this.$router.push(`/`)
       }
+    },
+    send_request() {
+      axios
+        .request({
+          url: this.url_value,
+          method: 'DELETE',
+          Headers: {
+            token: cookies.get('token')
+          }
+        })
+        .then((response) => {
+          this.delete_token()
+        })
+        .catch((error) => {
+          this.message = 'There is some error in log out'
+        })
     },
   },
   mounted() {
+    if(cookies.get('client_id')){
+      this.url_value = `${process.env.VUE_APP_BASE_DOMAIN}/api/client_login`
+    } else if (cookies.get('restaurant_id')){
+      this.url_value = `${process.env.VUE_APP_BASE_DOMAIN}/api/restaurant_login`
+    }
+
     // show component will be true if
     // cookies token and client id are present or
     // cookies token and restaurant id are present
@@ -33,16 +56,17 @@ export default {
       cookies.get(`token`) &&
       (cookies.get(`client_id`) || cookies.get(`restaurant_id`))
     ) {
-      this.show_component = true;
+      this.show_component = true
     }
   },
   data() {
     return {
-        // by default show component is false i.e logout button will not get displayed
+      // by default show component is false i.e logout button will not get displayed
       show_component: false,
-    };
+      url_value: undefined
+    }
   },
-};
+}
 </script>
 
 <style lang="scss" scoped></style>
