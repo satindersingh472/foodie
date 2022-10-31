a752674bf7499bf1332fd9e74a383585f7f717f0
satindersingh772 <92008199+satindersingh772@users.noreply.github.com>
2022-10-31 10:44:27 -0600

    fixed_edit_menu_bug

--- src/views/viewMenuRest.vue
+++ src/views/viewMenuRest.vue
@@ -56,9 +56,8 @@ export default {
   },
   methods: {
     /*get details will change the menu on the main menu screen after editing the item */
-get_details(index){
-    this.details.splice(index,1)
-    this.message = 'No items available'
+get_details(){
+    this.get_menu()
 },
     get_menu() {
       this.info = cookies.get(`restaurant_id`)
